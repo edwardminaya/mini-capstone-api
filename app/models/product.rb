cfeb40292e2c2f5b64b013f911f5ba767a866b4c
“Edward <eminaya49@gmail.com>
2023-03-06 14:28:23 -0600

    added new methods in model and tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if price <= 10
+      return true
+    else
+      return false
+    end
+  end
+
+  def tax
+    tax_amount = price * 0.09
+    return tax_amount
+  end
+
+  def total
+    total_cost = price + tax()
+  end
 end
