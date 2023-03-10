a5fd48e475f7826c1f51564df51cf9b1718f93fa
“Edward <eminaya49@gmail.com>
2023-03-10 12:36:04 -0600

    created order model

--- app/models/product.rb
+++ app/models/product.rb
@@ -17,14 +17,8 @@ class Product < ApplicationRecord
   end
 
   belongs_to :supplier
-  # def supplier
-  #   Supplier.find_by(id: supplier_id)
-  # end
-
   has_many :images
-  # def images
-  #   Image.where(product_id: id)
-  # end
+  has_many :orders
 
   # validates :name, presence: true
   # validates :name, uniqueness: true
