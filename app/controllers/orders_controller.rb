1d2e3dfd33e7923525464174fee3d02c92f3c102
“Edward <eminaya49@gmail.com>
2023-03-10 13:23:41 -0600

    created routes and controllers for create and show and tested with http request

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,18 @@
+class OrdersController < ApplicationController
+  def create
+    @order = Order.create(
+      user_id: params[:user_id],
+      product_id: params[:product_id],
+      quantity: params[:quantity],
+      subtotal: params[:subtotal],
+      tax: params[:tax],
+      total: params[:total],
+    )
+    render :show
+  end
+
+  def show
+    @order = Order.find_by(id: params[:id])
+    render :show
+  end
+end
