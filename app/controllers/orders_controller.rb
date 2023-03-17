9ec70844f607ae41a1d7fd57dadf16c84dfb120f
“Edward <eminaya49@gmail.com>
2023-03-17 11:38:36 -0600

    saving for now

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,22 +2,28 @@ class OrdersController < ApplicationController
   before_action :authenticate_user
 
   def create
-    product = Product.find_by(id: params[:product_id])
+    # product = Product.find_by(id: params[:product_id])
 
-    calculated_subtotal = product.price * params[:quantity].to_i
-    calculated_tax = calculated_subtotal * 0.09
-    calculated_total = calculated_subtotal + calculated_tax
+    # calculated_subtotal = product.price * params[:quantity].to_i
+    # calculated_tax = calculated_subtotal * 0.09
+    # calculated_total = calculated_subtotal + calculated_tax
+
+    # @order = Order.new(
+    #   user_id: current_user.id,
+    #   product_id: params[:product_id],
+    #   subtotal: calculated_subtotal,
+    #   tax: calculated_tax,
+    #   total: calculated_total,
+    # )
+    # @order.save
+
+    @carted_products = CartedProduct.where(status: "carted")
+    render :show
 
     @order = Order.new(
       user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
-      subtotal: calculated_subtotal,
-      tax: calculated_tax,
-      total: calculated_total,
+      subtotal: @carted_products.each do |x| {}
     )
-    @order.save
-    render :show
   end
 
   def show
