566d6dc0036958eff6467869b9c961da5f081cb6
“Edward <eminaya49@gmail.com>
2023-03-17 12:18:57 -0600

    returned order controller to original state

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,28 +2,20 @@ class OrdersController < ApplicationController
   before_action :authenticate_user
 
   def create
-    # product = Product.find_by(id: params[:product_id])
+    product = Product.find_by(id: params[:product_id])
 
-    # calculated_subtotal = product.price * params[:quantity].to_i
-    # calculated_tax = calculated_subtotal * 0.09
-    # calculated_total = calculated_subtotal + calculated_tax
-
-    # @order = Order.new(
-    #   user_id: current_user.id,
-    #   product_id: params[:product_id],
-    #   subtotal: calculated_subtotal,
-    #   tax: calculated_tax,
-    #   total: calculated_total,
-    # )
-    # @order.save
-
-    @carted_products = CartedProduct.where(status: "carted")
-    render :show
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
 
     @order = Order.new(
       user_id: current_user.id,
-      subtotal: @carted_products.each do |x| {}
+      product_id: params[:product_id],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
     )
+    @order.save
   end
 
   def show
