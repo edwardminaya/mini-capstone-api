8de1429079d2b424f8c7dbf4ab7d1fee40c7d474
“Edward <eminaya49@gmail.com>
2023-03-20 08:05:18 -0600

    updated create to show default carted status, created index to show only carted products from current user, created destroy action to change status from carted to removed in carted_products controller

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -4,10 +4,14 @@ class CartedProductsController < ApplicationController
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      status: params[:status],
+      status: "carted",
     )
-    @carted_product.save
-    render :show
+
+    if @carted_product.save
+      render :show
+    else
+      render json: { errors: @carted_product.errors.full_messages }
+    end
   end
 
   def index
@@ -17,5 +21,11 @@ class CartedProductsController < ApplicationController
       @carted_products = @carted_products.where(status: "carted")
     end
     render :index
+  end
+
+  def destroy
+    carted_product = current_user.carted_products.find_by(id: params[:id], status: "carted")
+    carted_product.update(status: "removed")
+    render json: { status: "Carted product successfully removed!" }
   end
 end
